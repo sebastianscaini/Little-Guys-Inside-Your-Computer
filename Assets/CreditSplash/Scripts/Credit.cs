db6e024e9bb0db60383b57d50551f33b8063a844
Sebastian Scaini <sebastianscaini@gmail.com>
2024-10-05 08:59:13 -0400

    Imported CreditsSplash

---
+++ Assets/CreditSplash/Scripts/Credit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour {
+
+    public string twitterLink;
+
+    private void OnMouseDown()
+    {
+        //open the URL supplied.
+        Application.OpenURL(twitterLink);
+    }
+}
---
+++ Assets/CreditSplash/Scripts/LoadManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManager : MonoBehaviour {
+
+    public float timeToWait;
+    public int sceneToLoad = 1;
+	
+    // Use this for initialization
+	void Start () {
+        Invoke("LoadNextScene", timeToWait);
+	}
+	
+    /// <summary>
+    /// Load the scene that was inputted.
+    /// </summary>
+	private void LoadNextScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
